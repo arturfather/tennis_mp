5a544265931ac6a21c32a454ce081eb1927784dc
Arthur Likhachev <arturfather@yandex.ru>
2024-10-25 19:10:22 +0300

    test

--- bot_checker.sh
+++ bot_checker.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Define the log file path
-LOG_FILE="/root/my_bots/football_bot/logs/logger.txt"
+LOG_FILE="/root/my_bots/tt_bot/logs/logger.txt"
 
 # Get the current time
 time_now=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Check if the bot process is running
-if ! pgrep -f "python football_bot.py" >/dev/null; then
+if ! pgrep -f "python tt_bot.py" >/dev/null; then
     echo "$time_now Bot is not running. Restarting..." >> "$LOG_FILE"
     
     # Activate the virtual environment
-    source /root/my_bots/football_bot/venv_soccer/bin/activate
+    source /root/my_bots/tt_bot/venv_tt/bin/activate
     
     # Change directory to the bot's location
-    cd /root/my_bots/football_bot/
+    cd /root/my_bots/tt_bot/
     
     # Restart the bot and redirect output
-    nohup python football_bot.py > /dev/null 2>> "$LOG_FILE" &
+    nohup python tt_bot.py > /dev/null 2>> "$LOG_FILE" &
 #else
 #   echo "$time_now Bot is already running." >> "$LOG_FILE"
 fi
